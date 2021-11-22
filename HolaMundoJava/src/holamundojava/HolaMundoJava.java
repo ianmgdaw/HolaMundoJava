e561b18c7f708540baa1387a6140f41e6e91e177
Ian MG <ianmg.daw@gmail.com>
2021-11-22 19:26:55 +0100

    Creando el proyecto HolaMundoJava

---
+++ HolaMundoJava/src/holamundojava/HolaMundoJava.java
@@ -0,0 +1,19 @@
+/*
+ * Proyecto HolaMundoJava - Archivo HolaMundoJava.java - Compañia DAW
+ * Licencia Creative Commons BY-NC-SA 4.0
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+package holamundojava;
+
+/**
+ *
+ * @author Ian Molina Guillo <ianmg.daw@gmail.com>
+ */
+public class HolaMundoJava {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
